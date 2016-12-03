36704bf7d91c3f9bc79afa76cf7fe2b018f36e4b
Mathew Kirschbaum <mkirsch42@gmail.com>
2016-12-03 07:31:08 -0500

    Modify drivetrain

--- driveAPI.h
+++ driveAPI.h
@@ -1,14 +1,21 @@
 #define ELEVATOR_SPEED 255
-#define ELEVATOR_REST 32
+#define ELEVATOR_REST 8
+
+#define R_FACTOR 1
+#define L_FACTOR -1
 
 void setL(int val) {
-	motor[left1] = motor[left2] = val;
+	motor[left1] = motor[left2] = val * L_FACTOR;
 }
 
 void setR(int val) {
-	motor[right1] = motor[right2] = val;
+	motor[right1] = motor[right2] = val * R_FACTOR;
 }
 
 void setE(int val) {
 	motor[elevator1] = motor[elevator2] = motor[elevator3] = motor[elevator4] = val;
+}
+
+void resetIme() {
+	SensorValue[imeE] = SensorValue[imeL] = SensorValue[imeR] = 0;
 }
